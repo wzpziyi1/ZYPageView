818d7831cab3c2685e20f4a512939dc0b3c238de
wzpziyi1 <1204473090@qq.com>
2017-05-31 16:36:57 +0800

    集成Alamofire

--- ZYPageView/Gift/ViewModel/ZYGiftVM.swift
+++ ZYPageView/Gift/ViewModel/ZYGiftVM.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ZYGiftVM: NSObject {
     
-    lazy var giftlistData : [ZYGiftPackage] = [GiftPackage]()
+    lazy var giftlistData : [ZYGiftPackage] = [ZYGiftPackage]()
     
 }
 
@@ -20,14 +20,14 @@ extension ZYGiftVM {
         
         if giftlistData.count != 0 { finishedCallback() }
         
-        ZYNetworkTool.requestData(.get, URLString: "http://qf.56.com/pay/v4/giftList.ios", parameters: ["type" : 0, "page" : 1, "rows" : 150], finishedCallback: { result in
+        ZYNetworkTool.requestData(.get, urlStr: "http://qf.56.com/pay/v4/giftList.ios", params: ["type" : 0, "page" : 1, "rows" : 150], completedCallback: { result in
             guard let resultDict = result as? [String : Any] else { return }
             
             guard let dataDict = resultDict["message"] as? [String : Any] else { return }
             
             for i in 0..<dataDict.count {
                 guard let dict = dataDict["type\(i+1)"] as? [String : Any] else { continue }
-                self.giftlistData.append(GiftPackage(dict: dict))
+                self.giftlistData.append(ZYGiftPackage(dict: dict))
             }
             
             self.giftlistData = self.giftlistData.filter({ return $0.t != 0 }).sorted(by: { return $0.t > $1.t })
